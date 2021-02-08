57403c1e72eda79141bbd7ff9232a0b72ee9b94d
Mamady Sillah <supurpisces310@msn.com>
2021-02-07 22:59:21 -0500

    another array expressions

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var myApp = angular.module("AngularApp", []);
 
 myApp.controller("HelloController", function($scope){
-$scope.expression = {
-    key1: 'welcome',
-    key2: "to",
-    key3: "tulane.com"
-}
+$scope.name = "Sally"
+$scope.marks = [43 , 49, 49, 44];
 });
